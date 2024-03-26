1db4ee4d393a2030fa849a8a98a028e1423b115d
goobergirl87 <isabelheidekorn@gmail.com>
2024-03-26 15:49:30 +0100

    Create a recipe example

--- hwca.js
+++ hwca.js
@@ -4,17 +4,32 @@ class Recipe {
   date = new Date();
   id = (Date.now() + "").slice(-10);
 
-  constructor(name, time, kind, wokeness, ingredients) {
-    this.name = name;
+  constructor(title, time, kind, wokeness, ingredients, preparation) {
+    this.title = title;
     this.time = time;
     this.kind = kind;
     this.wokeness = wokeness;
     this.ingredients = ingredients;
+    this.preparation = preparation;
   }
 }
 
 class Library {
   recipeLibrary = [];
+
+  newRecipe(e) {
+    let recipe;
+    recipe = new Recipe(title, time, kind, wokeness, ingredients, preparation);
+  }
 }
 
 const library = new Library();
+const brot = new Recipe(
+  "brot",
+  "zweiStunde",
+  "Backwaren",
+  "vegan",
+  ["380g Mehl", "300ml lauwarmes Wasser", "3g Trockenhefe"],
+  "fdsjfhaejkrbfj,dhsjakrhlfjvadns"
+);
+console.log(brot);
