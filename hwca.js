3f6dcbcfaac46f9b29946f2c5459d2aecda1198c
goobergirl87 <isabelheidekorn@gmail.com>
2024-04-27 14:56:05 +0200

    Change location of collaps togg

--- hwca.js
+++ hwca.js
@@ -51,6 +51,21 @@ class Library {
   }
 
   renderRecipe(recipe) {
+    let coll = document.getElementsByClassName("recipe-div");
+    let i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+
     let ingredientsFormatted = function () {
       let ingr = recipe.ingredients;
       return ingr.replaceAll(",", "<br>");
